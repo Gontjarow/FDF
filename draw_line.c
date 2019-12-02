7db2b91523d3e831be8e3434950a4292a09ea030
Niko Gontjarow <ngontjar@c1r5p5.hive.fi>
2019-12-02 19:04:42 +0200

    Major refactoring, includes commented debug code for drawing circle outlines.

--- draw_line.c
+++ draw_line.c
@@ -6,33 +6,38 @@
 /*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/01 17:47:55 by ngontjar          #+#    #+#             */
-/*   Updated: 2019/12/01 18:49:27 by ngontjar         ###   ########.fr       */
+/*   Updated: 2019/12/02 19:02:18 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 #include "fdf.h"
+#include <stdio.h>
+#include <time.h>
 
-int	draw_line(t_line *line)
+int	draw_line(t_shape *line)
 {
 	mlx_clear_window(g_env.mlx, g_env.win);
-	static double shiftx = 4;
-	static double shifty = 2;
-	double	dx = ((line->end.x + (shiftx += 4)) - line->start.x);
-	double	dy = ((line->end.y + (shifty += 2)) - line->start.y);
-	size_t	step = (ABS(dx) >= ABS(dy) ? ABS(dx) : ABS(dy));
-	dx /= step;
-	dy /= step;
+	t_xy	pixel;
+	t_xy	delta;
+	int		points;
 
-	size_t	i = 1;
-	double	x = line->start.x;
-	double	y = line->start.y;
-	while (i <= step)
+	// struct timespec tp;
+	// clock_gettime(CLOCK_REALTIME, &tp);
+	// line->data.x = line->pos.x + 300 * cos(tp.tv_nsec);
+	// line->data.y = line->pos.y + 300 * sin(tp.tv_nsec);
+
+	pixel = line->pos;
+	delta = (t_xy){line->data.x - line->pos.x, line->data.y - line->pos.y};
+	points = sqrt((delta.x * delta.x) + (delta.y * delta.y));
+	delta.x /= points;
+	delta.y /= points;
+	while (points > 0)
 	{
-		mlx_pixel_put(g_env.mlx, g_env.win, x, y, line->color);
-		x = x + dx;
-		y = y + dy;
-		i = i + 1;
+		mlx_pixel_put(g_env.mlx, g_env.win, pixel.x, pixel.y, line->color);
+		pixel.x += delta.x;
+		pixel.y += delta.y;
+		--points;
 	}
 	return (1);
 }
