55764837e76cfc04e33a62a22b0af157475e266e
Niko Gontjarow <ngontjar@c1r5p5.hive.fi>
2019-12-04 17:10:08 +0200

    Working main which properly reads the input file into memory. Does not draw the input.

--- main.c
+++ main.c
@@ -1,31 +1,33 @@
 #include "mlx.h"
 #include "fdf.h"
-
-
+#include <stdio.h>
+#include <fcntl.h>
 
 int			main(int argc, char **argv)
 {
-	int			**map;
-	t_xy		scale;
-	t_line		line;
-	t_rect		rect;
+	int			fd;
+	t_map		map;
+	t_shape		line;
 
-	if (argc == 2 && get_dimensions(argv[1], &scale))
+	if (argc == 2 && (fd = open(argv[1], O_RDONLY)) > 0)
 	{
-		map = malloc2d_int(scale);
-		ft_putnbr(scale.x); ft_putendl(" - width");
-		ft_putnbr(scale.y); ft_putendl(" - height");
+		parse_map(fd, &map);
+		close(fd);
+
+		ft_putnbr(map.size.x); ft_putendl(" - width");
+		ft_putnbr(map.size.y); ft_putendl(" - height");
 
 		g_env.mlx = mlx_init();
+		g_env.size.x = map.size.x * 50;
+		g_env.size.y = map.size.y * 50;
 		g_env.win = mlx_new_window(g_env.mlx,
-			scale.x * 100, scale.y * 100, "Cinematic 16:9 (tm)");
+			g_env.size.x, g_env.size.y, "Cinematic 16:9 (tm)");
 
-		line = (t_line){{10,10}, {40,40}, white};
+		line.pos = (t_xy){g_env.size.x / 2, g_env.size.y / 2};
+		line.data = (t_xy){line.pos.x + 90, line.pos.y + 90};
+		line.color = orange;
+
 		mlx_loop_hook(g_env.mlx, &draw_line, &line);
-
-		rect = (t_rect){{10, 20}, {scale.x, scale.y}, orange};
-		draw_rect(g_env.mlx, g_env.win, rect); // won't draw at the same time?
-
 		mlx_key_hook(g_env.win, &keyboard, NULL);
 		mlx_loop(g_env.mlx);
 	}
