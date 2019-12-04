8e3b9a8c494f007ffc5ae1af81d7aa607587e7b6
Niko Gontjarow <ngontjar@c1r5p5.hive.fi>
2019-12-04 17:09:30 +0200

    Changed draw_square to have correct parameters. This function will eventually be deleted, though.

--- draw_square.c
+++ draw_square.c
@@ -1,16 +1,16 @@
 #include "fdf.h"
 #include "mlx.h"
 
-int		draw_rect(void *mlx_ptr, void *win_ptr, t_rect rect)
+int		draw_rect(void *mlx_ptr, void *win_ptr, t_shape rect)
 {
-	size_t	w;
-	size_t	h;
+	int		w;
+	int		h;
 
 	h = 0;
-	while (h < rect.size.y)
+	while (h < rect.data.y)
 	{
 		w = 0;
-		while (w < rect.size.x)
+		while (w < rect.data.x)
 		{
 			mlx_pixel_put(mlx_ptr, win_ptr,
 				rect.pos.x + w, rect.pos.y + h, rect.color);
