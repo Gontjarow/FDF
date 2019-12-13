8d3208b9cc9cc77e1c9d0525cda9d75bef24e343
Niko Gontjarow <ngontjar@c1r6p20.hive.fi>
2019-12-13 21:43:43 +0200

    Fixed a really bad over-allocation and segfault.

--- malloc2d_xyz.c
+++ malloc2d_xyz.c
@@ -6,7 +6,7 @@
 /*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/01 12:51:33 by ngontjar          #+#    #+#             */
-/*   Updated: 2019/12/13 18:13:40 by ngontjar         ###   ########.fr       */
+/*   Updated: 2019/12/13 20:45:35 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 t_xyz	**malloc2d_xyz(t_xy size)
 {
 	t_xyz	**rows;
-	int		x;
 	int		y;
 
 	if ((rows = (t_xyz **)malloc(size.y * sizeof(t_xyz **))))
@@ -23,13 +22,9 @@ t_xyz	**malloc2d_xyz(t_xy size)
 		y = 0;
 		while (y < size.y)
 		{
-			x = 0;
-			while (x < size.x)
-			{
-				if (NULL == (rows[x] = (t_xyz *)malloc(size.x * sizeof(t_xyz))))
-					die("malloc failed in malloc2dxyz");
-				++x;
-			}
+			rows[y] = (t_xyz *)malloc(size.x * sizeof(t_xyz));
+			if (rows[y] == NULL)
+				die("malloc failed in malloc2dxyz");
 			++y;
 		}
 	}
